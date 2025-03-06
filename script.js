2829c8775a9527017d19f55207addbeb8873a89b
aankittt <ankithackerank22@gmail.com>
2025-03-06 17:06:07 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuList=document.getElementById("menuList");
+
+function toggleMenu() {
+    if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+      menuList.style.maxHeight = "300px";
+    } else {
+      menuList.style.maxHeight = "0px";
+    }
+  }
+
+  
